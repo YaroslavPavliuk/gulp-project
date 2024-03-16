98304d8f7d7ab1eb2eed5bb1091f8b10484ea566
Yaroslav Pavliuk <88543626+YaroslavPavliuk@users.noreply.github.com>
2024-03-16 17:24:53 +0200

    return convert fonts

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 // FONTS
 const ttf2woff2 = require('gulp-ttf2woff2');
+const fonter = require('gulp-fonter-fix');
 // JS
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
@@ -84,11 +85,74 @@ const imgBuild = () => {
     .pipe(dest('./build/img/'))
 }
 // Fonts dev
-const fonts = () => {
-  return src('./src/fonts/**/*.ttf')
-    .pipe(ttf2woff2())
-    .pipe(dest('./app/fonts/'))
+const srcFolder = './src';
+const destFolder = './app';
+
+const convertFonts = () => {
+	return src(['./src/fonts/*.otf', './src/fonts/*.eot'])
+  .pipe(
+    fonter({
+      formats: ['ttf'],
+    })
+  )
+  .pipe(dest('./src/fonts/'))
+  .pipe(src('./src/fonts/*.ttf'))
+  .pipe(ttf2woff2())
+  .pipe(dest('./app/fonts'))
 }
+
+const stylesFonts = () => {
+	let fontsFile = `${srcFolder}/scss/_fonts.scss`;
+
+	fs.readdir(`${destFolder}/fonts/`, function (err, fontsFiles) {
+		if (fontsFiles) {
+			
+				fs.writeFile(fontsFile, '', cb);
+				let newFileOnly;
+				for (var i = 0; i < fontsFiles.length; i++) {
+					let fontFileName = fontsFiles[i].split('.')[0];
+					if (newFileOnly !== fontFileName) {
+						let fontName = fontFileName.split('-')[0]
+							? fontFileName.split('-')[0]
+							: fontFileName;
+						let fontWeight = fontFileName.split('-')[1]
+							? fontFileName.split('-')[1]
+							: fontFileName;
+						if (fontWeight.toLowerCase() === 'thin') {
+							fontWeight = 100;
+						} else if (fontWeight.toLowerCase() === 'extralight') {
+							fontWeight = 200;
+						} else if (fontWeight.toLowerCase() === 'light') {
+							fontWeight = 300;
+						} else if (fontWeight.toLowerCase() === 'medium') {
+							fontWeight = 500;
+						} else if (fontWeight.toLowerCase() === 'semibold') {
+							fontWeight = 600;
+						} else if (fontWeight.toLowerCase() === 'bold') {
+							fontWeight = 700;
+						} else if (
+							fontWeight.toLowerCase() === 'extrabold' ||
+							fontWeight.toLowerCase() === 'heavy'
+						) {
+							fontWeight = 800;
+						} else if (fontWeight.toLowerCase() === 'black') {
+							fontWeight = 900;
+						} else {
+							fontWeight = 400;
+						}
+						fs.appendFile(
+							fontsFile,
+							`@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`,
+							cb
+						);
+						newFileOnly = fontFileName;
+					}
+				}
+		}
+	});
+  return src('./src')
+	function cb() {}
+};
 // FONTS build
 const fontsBuild = () => {
   return src('./app/fonts/**/*')
@@ -155,6 +219,6 @@ const watchFiles = () => {
   watch('./src/js/**/*.js', scripts);
 }
 // default task
-exports.default = series(clean, parallel(pages, scripts, fonts, imgToApp), styles, watchFiles); // GULP start
+exports.default = series(clean, parallel(pages, scripts, convertFonts, imgToApp),stylesFonts, styles, watchFiles); // GULP start
 exports.build = parallel(pagesBuild, stylesBuild, imgBuild, fontsBuild, scriptsBuild); // GULP build
 exports.zipBuild = zipBuild; // GULP zip build project
